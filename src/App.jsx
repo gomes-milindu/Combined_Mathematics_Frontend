f0701bd2fd62384f2824155364bdac472cd77371
gomes-milindu <gomesmilindu@gmail.com>
2025-12-04 22:46:50 +0530

    created a card

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 import Header from './components/Header'
 import HeroText from './components/HomePage/HeroText'
 import GlassBox from './components/HomePage/GlassBox'
+import Card from './components/HomePage/Card'
+import GridLayout from './components/layout/GridLayout'
 
 function App() {
 
 
   return (
     <>
+      {/* <GridLayout /> */}
       <Header />
       <HeroText />
       <GlassBox />
+      <Card />
     </>
   )
 }
---
+++ src/components/layout/GridLayout.jsx
@@ -0,0 +1,25 @@
+// src/components/GridOverlay.jsx
+function GridLayout() {
+  // We don't need real content, just columns
+  const cols = Array.from({ length: 12 }, (_, i) => i); // max (desktop) count
+
+  return (
+    <div className="fixed inset-0 z-100 pointer-events-none">
+      {/* page width container */}
+      <div className="w-full h-full flex justify-center">
+        <div className="w-full h-full max-w-7xl px-4">
+          <div className="grid grid-cols-4 md:grid-cols-6 xl:grid-cols-12 h-full gap-x-2 lg:gap-x-6">
+            {cols.map((i) => (
+              <div
+                key={i}
+                className="bg-pink-200/30"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GridLayout;
