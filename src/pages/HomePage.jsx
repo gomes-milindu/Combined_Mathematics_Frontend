a6074f3c2b18570d02aa38d52e4012bd1a92e71a
gomes-milindu <gomesmilindu@gmail.com>
2025-12-26 14:26:43 +0530

    change spelling mistake

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import GlassBox from "../components/HomePage/GlassBox";
 import HeroText from "../components/HomePage/HeroText";
 import AboutTeacher from "../sections/HomePage/AboutTeacher";
-import CardSection from "../sections/HomePage/cardSection";
+import CardSection from "../sections/HomePage/CardSection";
 import JoinClassSection from "../sections/HomePage/JoinClassSection";
 import PopularCourses from "../sections/HomePage/PopularCourses";
 import ResultCardSection from "../sections/HomePage/ResultCardSection";
