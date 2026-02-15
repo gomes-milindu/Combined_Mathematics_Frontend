0e63bd4c37e43d6c3ecd0cbe0b5f7e5fdc1084ca
gomes-milindu <gomesmilindu@gmail.com>
2026-02-15 17:06:59 +0530

    updated

--- src/components/AdminPage/Dashborad.jsx
+++ src/components/AdminPage/Dashborad.jsx
@@ -1,57 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export function Dashboard() {
+  const [countStudent, setCountStudent] = useState();
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/dashboard/").then((res) => {
+      setCountStudent(res.data);
+      console.log(res.data);
+    });
+  }, []);
   return (
     <>
-      <div className="w-full flex flex-col justify-center items-center ">
-        <div className="w-[90%] h-50 bg-red-300 flex flex-row justify-around gap-5">
-          <div className="bg-green-400 w-[20%]">
-            <div className="text-center">Total Students</div>
-            <div className="text-center">120</div>
-          </div>
-          <div className="bg-green-400 w-[20%]">
-            <div className="text-center">Total Income</div>
-            <div className="text-center">$1200</div>
-          </div>
-          <div className="bg-green-400 w-[20%]">
-            <div className="text-center">Total Net Profit</div>
-            <div className="text-center">$120</div>
-          </div>
-        </div>
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="">Total Students: </div>
+        <div className="text-2xl font-bold">{countStudent?.totalStudents}</div>
 
-        <div className="w-[90%] ">
-          <div className="overflow-x-auto">
-  <table className="min-w-full border border-gray-300 text-sm text-center">
-    
-    <thead className="bg-gray-100">
-      <tr>
-        <th className="border px-4 py-2">Institute</th>
-        <th className="border px-4 py-2">2028 T</th>
-        <th className="border px-4 py-2">2029 T</th>
-        <th className="border px-4 py-2">2030 T</th>
-        <th className="border px-4 py-2">2030 R</th>
-        <th className="border px-4 py-2">2030 P</th>
-        <th className="border px-4 py-2">Total</th>
-        <th className="border px-4 py-2">Net Profit</th>
-      </tr>
-    </thead>
+        <div className="">Total Income: </div>
+        <div className="text-2xl font-bold">{countStudent?.totalIncome}</div>
 
-    <tbody>
-      <tr className="hover:bg-gray-50">
-        <td className="border px-4 py-2">Samathwee</td>
-        <td className="border px-4 py-2">2</td>
-        <td className="border px-4 py-2">3</td>
-        <td className="border px-4 py-2">10</td>
-        <td className="border px-4 py-2">1</td>
-        <td className="border px-4 py-2">10</td>
-        <td className="border px-4 py-2 font-semibold">26</td>
-        <td className="border px-4 py-2 text-green-600 font-semibold">
-          $10000
-        </td>
-      </tr>
-    </tbody>
+        <div className="">Net Profit: </div>
+        <div className="text-2xl font-bold">{countStudent?.netProfit}</div>
+      </div>
 
-  </table>
-</div>
-        </div>
+      <div className="">
+        <table>
+          <thead>
+            <th>Institue</th>
+            <th>Batch</th>
+            <th>Total</th>
+            <th>Revenue</th>
+          </thead>
+
+          <tbody>
+            {countStudent?.activeCounts?.map((item, index) => (
+              <tr key={index}>
+                <td>{item.institute}</td>
+                <td>{item.batch}</td>
+                <td>{item.totalStudents}</td>
+                <td>5000</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
