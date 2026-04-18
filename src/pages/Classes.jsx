d3b6e21ee4ff1994bd29cea801267a97da3a2cfb
gomes-milindu <gomesmilindu@gmail.com>
2026-04-18 23:37:41 +0530

    update clss card

--- src/pages/Classes.jsx
+++ src/pages/Classes.jsx
@@ -2,13 +2,13 @@ import ClassCard from "../components/ClassesPage/ClassCard"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import NavBar from "../components/NavBar"
-import HeaderImage from "../assets/Hero/classCardHeader.jpg"
+
 import ClassCardSection from "../sections/ClassPage/ClassCardSection"
 
 function Classes(){
     return(
         <>
-            <Header backgroundImage= "bg-[url(./assets/Hero/classCardHeader.jpg)]"/>
+            <Header bgImage= "https://afablbkjnkbxwieszbtb.supabase.co/storage/v1/object/public/Classes%20Page/classCardHeader.jpg"/>
             <NavBar />
 
             <div className="w-full flex justify-center items-center mt-20">
